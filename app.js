a37d4d42abc48d396aeed89930d3bebfee3379cf
Siravitt <siravitt.tan@gmail.com>
2024-02-26 02:13:14 +0700

    start server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const app = express.Router()
+const app = express()
 
 app.get("/hello", (req,res,next) => {
     res.send("<div>Hello world</div>")
 })
 
-app.listen(3000);
+app.listen(3000, ()=>console.log("server running on port 3000"));
